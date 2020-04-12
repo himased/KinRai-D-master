5123fd489dca68c7f3e351177d1137d17e5d64b1
himased1214 <chanakan_c@elearning.cmu.ac.th>
2020-04-12 13:18:56 +0700

    Updates

---
+++ precache-manifest.ec43460fe2bf06f82170a407a66c0d63.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d5b55f37d09955becf9ac54af318fd",
+    "url": "/KinRai-D-master/index.html"
+  },
+  {
+    "revision": "7ece94d20ba9c2da59c3",
+    "url": "/KinRai-D-master/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "896cab2499dda261c441",
+    "url": "/KinRai-D-master/static/css/main.dbef194f.chunk.css"
+  },
+  {
+    "revision": "7ece94d20ba9c2da59c3",
+    "url": "/KinRai-D-master/static/js/2.215f3be5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KinRai-D-master/static/js/2.215f3be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896cab2499dda261c441",
+    "url": "/KinRai-D-master/static/js/main.e6807c3a.chunk.js"
+  },
+  {
+    "revision": "4844b9935026d2308377",
+    "url": "/KinRai-D-master/static/js/runtime-main.a57ec4f7.js"
+  },
+  {
+    "revision": "48e0803d0e6034fa0417c04388fdb828",
+    "url": "/KinRai-D-master/static/media/Logo1.48e0803d.png"
+  },
+  {
+    "revision": "d7a2b27ba201f14338262f1b768c2dcb",
+    "url": "/KinRai-D-master/static/media/bg.d7a2b27b.jpg"
+  }
+]);
